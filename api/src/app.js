17b234cc33cd79d91aa1ffccfdaaafd17fb75200
dante-dlcio <erio-donalicio@hotmail.com>
2023-02-24 18:22:01 +0100

    testing deployment

--- api/src/app.js
+++ api/src/app.js
@@ -18,7 +18,7 @@ server.use((req, res, next) => {
   //localHost
   //res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
   //deployed
-  res.header('Access-Control-Allow-Origin', 'https://foods-backend.up.railway.app/'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', 'https://foods-backend.up.railway.app/' || 'http://localhost:3000'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
